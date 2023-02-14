1c6f8eeceb1ae0720534cf10662b7562e26346f7
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-14 12:21:09 +0300

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// To see if the day is a weekend
+Console.WriteLine("Input the number");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+    Console.WriteLine("Yeah, today is the weekend");
+    else if (a > 0 && a < 8)
+        Console.WriteLine("Sorry, today is not the weekend");
+    else 
+    Console.WriteLine("It is not a weekday");
