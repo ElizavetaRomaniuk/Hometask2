1553436ceeb054d9f066b9cdcb70f31ab9bd5aa2
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-14 12:11:19 +0300

    Task 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// To see the third digit of the number
+Console.WriteLine("Input the number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+    Console.WriteLine(a % 10);
+    else if (a > 999)
+        Console.WriteLine((a / 100) % 10);
+else 
+    Console.WriteLine("It is not a three-digit number");
