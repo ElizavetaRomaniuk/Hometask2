d2c7b19a36c7ca2e49828d9296323a6a55a8f899
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-14 11:53:38 +0300

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input the number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+    int result = (a % 100) / 10;
+    Console.WriteLine(result);
+}
+    else 
+        Console.WriteLine("It is not a three-digit number");
